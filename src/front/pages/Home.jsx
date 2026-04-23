8e4e8fc91604eb2101de603378a127508bd6fea6
HRLopezP <hrlp843@gmail.com>
2026-04-23 23:33:05 +0000

    Componente Rules

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,27 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Importante para la navegación
 import "../styles/home.css";
 
 export const Home = () => {
     return (
-        <div className="container-fluid pb-5">
+        <div className="container-fluid pb-5 animate__animated animate__fadeIn">
             <div className="row justify-content-center mt-4">
                 <div className="col-12 col-lg-9">
-
-                    <div className="banner-container">
-                        {/* La Imagen de Fondo */}
+                    <div className="banner-container shadow-lg">
                         <img
                             src="https://res.cloudinary.com/dowqpndnq/image/upload/v1776978362/Home_elite_idjkpm.png"
                             alt="Mundial Élite"
                             className="banner-img"
                         />
 
-                        {/* Título Superior */}
                         <div className="banner-title">
                             <h1 className="title-main">¡MUNDIAL ÉLITE!</h1>
                             <p className="title-sub">¡TU CAMINO A LA GLORIA!</p>
                         </div>
 
-                        {/* Contenido Lateral Izquierdo */}
                         <div className="banner-overlay-left d-none d-md-block">
                             <h2 className="text-elite">
                                 ¡DEMUESTRA<br />que eres el<br /><span>ÉLITE!</span>
@@ -31,14 +28,18 @@ export const Home = () => {
                             </p>
 
                             <div className="banner-buttons">
-                                <button className="btn btn-outline-info rounded-pill px-4 mt-1 border-0">
-                                    <i className="fas fa-gavel reglamento-icon"></i>
+                                {/* Ahora el botón es un enlace real */}
+                                <Link to="/ranking" className="btn btn-predict">
+                                    ¡HACER MIS PREDICCIONES!
+                                </Link>
+                                
+                                <button className="btn btn-link text-info text-decoration-none p-0 mt-2">
+                                    <i className="fas fa-gavel me-2"></i>
                                     Ver Reglamento ÉLITE
                                 </button>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
